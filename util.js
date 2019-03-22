d9a95f6c052a5e34a143278ffa6e8dcc7a9df248
João Antônio <jacs@cin.ufpe.br>
2019-03-22 12:13:36 -0300

    lets go lunch

--- util.js
+++ util.js
@@ -1,11 +1,25 @@
 function ParseRequest(msg) {
     if(!msg || msg.text == undefined)
         return null;
+    
+    var regex = RegExp('([+-]*[1-9]+?[0-9]*d[1-9]+?[0-9]*)','ig');
+    var auxArray;
+    
+    var dies = [];
+    while((auxArray = regex.exec(msg.text)) != null) {
+        dies.push(auxArray[0]);
+    }
+
+    regex = RegExp('([+-][1-9]+?[0-9]*(?![0-9]*d))','ig');
+    var modifiers = [];
+    while((auxArray = regex.exec(msg.text)) != null) {
+        modifiers.push(auxArray[0]);
+    }
 
     var ret = {
-        'command': (/(\/[a-z]+)/i).exec(msg.text).slice(1,-3),
-        'dies': (/([+-]*[1-9]+?[0-9]*d[1-9]+?[0-9]*)/ig).exec(msg.text).slice(1,-3),
-        'modifiers': (/([+-][1-9]+?[0-9]*(?![0-9]*d))/ig).exec(msg.text).slice(1,-3)
+        'command': (/(\/[a-z]+)/i).exec(msg.text)[0],
+        'dies': dies,
+        'modifiers': modifiers
     };
 
     return ret;
@@ -13,9 +27,9 @@ function ParseRequest(msg) {
 
 function ParseDie(die) {
     var ret = {
-        'modifier': parseInt((/^[+-]/).exec(die)+'1'),
-        'quantity': parseInt((/[0-9]+/).exec(die)),
-        'faces': parseInt((/[0-9]+$/).exec(die))
+        'modifier': parseInt((/^[+-]/).exec(die)[0]+'1'),
+        'quantity': parseInt((/[0-9]+/).exec(die)[0]),
+        'faces': parseInt((/[0-9]+$/).exec(die)[0])
     }
 
     return ret;
