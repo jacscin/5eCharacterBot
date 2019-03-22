1c2702314d6205737d7139ea670cb5472ab0e524
João Antônio <jacs@cin.ufpe.br>
2019-03-22 11:34:20 -0300

    Style correction

--- web.js
+++ web.js
@@ -7,18 +7,18 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  res.json({ version: packageInfo.version });
+    res.json({ version: packageInfo.version });
 });
 
 var server = app.listen(process.env.PORT, "0.0.0.0", () => {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log('Web server started at http://%s:%s', host, port);
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log('Web server started at http://%s:%s', host, port);
 });
 
 module.exports = (bot) => {
-  app.post('/' + bot.token, (req, res) => {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
-  });
+    app.post('/' + bot.token, (req, res) => {
+        bot.processUpdate(req.body);
+        res.sendStatus(200);
+    });
 };
